2fc806b87e42e7a3a451d291bad41e87809abf47
thislooksfun <thislooksfun@users.noreply.github.com>
2014-04-29 18:32:41 -0500

    Update NightMode.js

--- JavaScript/NightMode.js
+++ JavaScript/NightMode.js
@@ -3,33 +3,35 @@ window.nightMode = {};
 //Functions
 nightMode.init = function()
 {
-  this.pageElements = [];
-  var eleNum = 0; //Element number
-  
-  //Format: {'element': Element, 'background': {'day': Day background color, 'night': Night background color], 'text': {'day': Day text color, 'night': Night text color}};
-  this.pageElements[eleNum++] = {'element': document.body,                                    'background': {'day': '#f1f1f1', 'night': '#0e0e0e'}};
-  this.pageElements[eleNum++] = {'element': document.getElementById('yt-masthead-container'), 'background': {'day': '#fff',    'night': '#000'}};
-  this.pageElements[eleNum++] = {'element': document.getElementById('search-btn'),            'background': {'day': '#fff',    'night': '#000'}};
-  this.pageElements[eleNum++] = {'element': document.getElementById('masthead-search-terms'), 'background': {'day': '#fff',    'night': '#000'}, 'text': {'day': '#666', 'night': '#999'}};
-  this.pageElements[eleNum++] = {'element': document.getElementById('appbar-guide-button'),   'background': {'day': '', 'night': '#070707'}};
-  
-  if (!this.nightButton) {
-    this.nightButton = document.createElement('button');
-    this.nightButton.setAttribute('id', 'nightButton');
-    this.nightButton.setAttribute('onClick', 'nightMode.toggle()');
-    this.nightButton.setAttribute('class', 'yt-uix-button yt-uix-button-default yt-uix-button-size-default')
-    this.nightButton.innerHTML = 'Night Mode';
-    document.getElementById('yt-masthead-content').appendChild(this.nightButton);
+  if (!this.initalized) {
+    this.pageElements = [];
+    var eleNum = 0; //Element number
     
-    var cssNight = document.createElement('link');
-    cssNight.setAttribute('type', 'text/css');
-    cssNight.setAttribute('rel', 'stylesheet');
-    cssNight.setAttribute('href', 'https://raw.githubusercontent.com/thislooksfun/YouTubeNightMode/master/css/mainNight.css')
-    document.getElementById('yt-masthead-content').appendChild(cssNight);
+    //Format: {'element': Element, 'background': {'day': Day background color, 'night': Night background color], 'text': {'day': Day text color, 'night': Night text color}};
+    this.pageElements[eleNum++] = {'element': document.body,                                    'background': {'day': '#f1f1f1', 'night': '#0e0e0e'}};
+    this.pageElements[eleNum++] = {'element': document.getElementById('yt-masthead-container'), 'background': {'day': '#fff',    'night': '#000'}};
+    this.pageElements[eleNum++] = {'element': document.getElementById('search-btn'),            'background': {'day': '#fff',    'night': '#000'}};
+    this.pageElements[eleNum++] = {'element': document.getElementById('masthead-search-terms'), 'background': {'day': '#fff',    'night': '#000'}, 'text': {'day': '#666', 'night': '#999'}};
+    this.pageElements[eleNum++] = {'element': document.getElementById('appbar-guide-button'),   'background': {'day': '', 'night': '#070707'}};
+    
+    if (!this.nightButton) {
+      this.nightButton = document.createElement('button');
+      this.nightButton.setAttribute('id', 'nightButton');
+      this.nightButton.setAttribute('onClick', 'nightMode.toggle()');
+      this.nightButton.setAttribute('class', 'yt-uix-button yt-uix-button-default yt-uix-button-size-default')
+      this.nightButton.innerHTML = 'Night Mode';
+      document.getElementById('yt-masthead-content').appendChild(this.nightButton);
+      
+      var cssNight = document.createElement('link');
+      cssNight.setAttribute('type', 'text/css');
+      cssNight.setAttribute('rel', 'stylesheet');
+      cssNight.setAttribute('href', 'https://raw.githubusercontent.com/thislooksfun/YouTubeNightMode/master/css/mainNight.css')
+      document.getElementById('yt-masthead-content').appendChild(cssNight);
+    }
+    
+    this.isNight = false;
+    this.initalized = true;
   }
-  
-  this.isNight = false;
-  this.initalized = true;
 }
 
 nightMode.setNight = function()

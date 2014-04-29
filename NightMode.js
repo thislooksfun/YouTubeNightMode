ddae341273016436f59ca5d468b6d2efccd5ce0f
thislooksfun <thislooksfun@users.noreply.github.com>
2014-04-29 16:08:47 -0500

    Update NightMode.js

--- NightMode.js
+++ NightMode.js
@@ -6,10 +6,10 @@ nightMode.init = function()
   this.pageElements = [];
   
   //Format: ['element': Element, 'background': ['day': Day background color, 'night': Night background color], 'text': ['day': Day text color, 'night': Night text color]];
-  this.pageElements[0] = ['element': document.body,
-    'background': {
-      'day': '#f1f1f1',
-      'night': '#0e0e0e'
+  this.pageElements[0] = ["element": document.body,
+    "background": {
+      "day": '#f1f1f1',
+      "night": '#0e0e0e'
       }
     ];
   this.pageElements[1] = ['element': document.getElementById('yt-masthead-container'), 'background': {'day': '#fff',    'night': '#000'}];

a5265391a48e22df11ad33d37f5b2b9a8fbe2b3e
thislooksfun <thislooksfun@users.noreply.github.com>
2014-04-29 11:19:28 -0500

    Update NightMode.js

--- NightMode.js
+++ NightMode.js
@@ -9,16 +9,22 @@ nightMode.init = function() {
   
   alert('Color = ' + this.pageElements[0].style.color +'\nBackground = ' + this.pageElements[0].style.background);
   alert('Background = ' + document.body.style.background);
-  
-  document.body.style.background = '#000'
 }
 
 nightMode.toggleNight = function() {
-  
+  document.body.style.background = '#0e0e0e'
 }
 
 nightMode.toggleDay = function() {
-  
+  document.body.style.background = '#f1f1f1'
 }
 
 nightMode.init();
+
+nightMode.toggleNight();
+
+alert('Night mode');
+
+nightMode.toggleDay();
+
+alert('Day mode');

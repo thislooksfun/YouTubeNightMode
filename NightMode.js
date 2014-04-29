19b6a8f62d84f1d58788a3392061bcf6860ecb6e
thislooksfun <thislooksfun@users.noreply.github.com>
2014-04-29 12:42:00 -0500

    Update NightMode.js

--- NightMode.js
+++ NightMode.js
@@ -6,11 +6,11 @@ nightMode.init = function() {
   
   this.pageElements = [];
   
-  //Format: [Element, Day color, Night color]
+  //Format: [Element, [Day background color, Night background color], [Day text color, Night text color]];
   this.pageElements[0] = [document.body, '#f1f1f1', '#0e0e0e'];
-  this.pageElements[1] = [document.getElementById('yt-masthead-container'), '#fff', '#000'];
-  this.pageElements[2] = [document.getElementById('search-btn'), '#fff', '#000'];
-  this.pageElements[3] = [document.getElementById('masthead-search-terms'), '#fff', '#000'];
+  this.pageElements[1] = [document.getElementById('yt-masthead-container'), ['#fff', '#000'];
+  this.pageElements[2] = [document.getElementById('search-btn'), ['#fff', '#000'];
+  this.pageElements[3] = [document.getElementById('masthead-search-terms'), ['#fff', '#000'], ['#666' '#999']];
   
   
   this.isNight = false;
@@ -18,7 +18,10 @@ nightMode.init = function() {
 
 nightMode.setNight = function() {
   for (var i = 0; i < this.pageElements.length; i++) {
-    this.pageElements[i][0].style.background = this.pageElements[i][2]
+    this.pageElements[i][0].style.background = this.pageElements[i][1][1]
+    if (this.pageElements[i][2]) {
+      this.pageElements[i][0].style.color = this.pageElements[i][2][1]
+    }
   }
   alert('Night mode');
   
@@ -27,7 +30,10 @@ nightMode.setNight = function() {
 
 nightMode.setDay = function() {
   for (var i = 0; i < this.pageElements.length; i++) {
-    this.pageElements[i][0].style.background = this.pageElements[i][1]
+    this.pageElements[i][0].style.background = this.pageElements[i][1][0]
+    if (this.pageElements[i][2]) {
+      this.pageElements[i][0].style.color = this.pageElements[i][2][0]
+    }
   }
   alert('Day mode');
   

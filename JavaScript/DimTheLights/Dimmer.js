9adc6e7bfb4f8f067a2a272e39f2d50ba250ab24
thislooksfun <thislooksfun@users.noreply.github.com>
2014-04-30 17:11:23 -0500

    Update Dimmer.js

--- JavaScript/DimTheLights/Dimmer.js
+++ JavaScript/DimTheLights/Dimmer.js
@@ -77,7 +77,7 @@ dimmer.checkSize = function()
     var match = /(watch-small|watch-medium|watch-medium-540|watch-large)/;
     this.oldClass = match.exec(player.className)[0];
     
-    this.positionDivs();
+    dimmer.positionDivs();
   }
 }
 
